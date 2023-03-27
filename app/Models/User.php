a6695d3b0143ce54464e532162cd95cf076177f3
AirelJavier <airel.javier.ft20@mail.umy.ac.id>
2023-03-27 21:37:52 +0700

    env, mvc, route, relationship

--- app/Models/User.php
+++ app/Models/User.php
@@ -40,5 +40,12 @@ class User extends Authenticatable
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
+
+        'is_admin' => 'boolean'
     ];
+
+    public function todos()
+    {
+        return $this->hasMany(Todo::class);
+    }
 }
